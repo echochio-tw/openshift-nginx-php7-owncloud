80ff2f0242b84da1cd95df7a91720d6f24f11fc1
chio9.chio9 <chio-nzgft@users.noreply.github.com>
2017-06-20 22:16:12 +0800

    Create autoconfig.php

---
+++ etc/autoconfig.php
@@ -0,0 +1,21 @@
+<?php
+define("DIRECTORY",$_SERVER['OPENSHIFT_DATA_DIR'] );
+define("DBNAME",$_SERVER['OPENSHIFT_APP_NAME'] );
+define("DBTYPE",'mysql' );
+define("DBUSER",$_SERVER['OPENSHIFT_MYSQL_DB_USERNAME'] );
+define("DBPASS",$_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD'] );
+define("DBHOST",$_SERVER['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_SERVER['OPENSHIFT_MYSQL_DB_PORT'] );
+define("ADMIN_PASSWORD_FILE", $_SERVER['OPENSHIFT_DATA_DIR'] . '/.initial_owncloud_password' );
+$AUTOCONFIG = array(
+     'installed' => false,
+     'dbtype' => DBTYPE,
+     'dbtableprefix' => 'oc_',
+     'adminlogin' => 'admin',
+     'adminpass' => trim(array_shift(file(ADMIN_PASSWORD_FILE))),
+     'directory' => DIRECTORY,
+     'dbname' => DBNAME,
+     'dbuser' => DBUSER,
+     'dbpass' => DBPASS,
+     'dbhost' => DBHOST,
+  );
+?>
