e366ce1a3a4a3d1ca6a2e2d540ba4fb463df35f2
chio9.chio9 <chio-nzgft@users.noreply.github.com>
2017-06-21 07:08:06 +0800

    Update autoconfig.php

--- etc/autoconfig.php
+++ etc/autoconfig.php
@@ -5,13 +5,12 @@ define("DBTYPE",'mysql' );
 define("DBUSER",$_SERVER['OPENSHIFT_MYSQL_DB_USERNAME'] );
 define("DBPASS",$_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD'] );
 define("DBHOST",$_SERVER['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_SERVER['OPENSHIFT_MYSQL_DB_PORT'] );
-define("ADMIN_PASSWORD_FILE", $_SERVER['OPENSHIFT_REPO_DIR'] . 'ok' );
 $AUTOCONFIG = array(
      'installed' => false,
      'dbtype' => DBTYPE,
      'dbtableprefix' => 'oc_',
      'adminlogin' => 'admin',
-     'adminpass' => trim(array_shift(file(ADMIN_PASSWORD_FILE))),
+     'adminpass' => 'admin',
      'directory' => DIRECTORY,
      'dbname' => DBNAME,
      'dbuser' => DBUSER,
