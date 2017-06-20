a4e2f96af8df5016b240099c1a5c121ce9141352
chio9.chio9 <chio-nzgft@users.noreply.github.com>
2017-06-21 07:03:47 +0800

    Update autoconfig.php

--- etc/autoconfig.php
+++ etc/autoconfig.php
@@ -1,11 +1,11 @@
 <?php
-define("DIRECTORY",$_SERVER['OPENSHIFT_DATA_DIR']. '/www' );
+define("DIRECTORY",$_SERVER['OPENSHIFT_DATA_DIR']. 'www' );
 define("DBNAME",$_SERVER['OPENSHIFT_APP_NAME'] );
 define("DBTYPE",'mysql' );
 define("DBUSER",$_SERVER['OPENSHIFT_MYSQL_DB_USERNAME'] );
 define("DBPASS",$_SERVER['OPENSHIFT_MYSQL_DB_PASSWORD'] );
 define("DBHOST",$_SERVER['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_SERVER['OPENSHIFT_MYSQL_DB_PORT'] );
-define("ADMIN_PASSWORD_FILE", $_SERVER['OPENSHIFT_REPO_DIR'] . '/ok' );
+define("ADMIN_PASSWORD_FILE", $_SERVER['OPENSHIFT_REPO_DIR'] . 'ok' );
 $AUTOCONFIG = array(
      'installed' => false,
      'dbtype' => DBTYPE,
